67aed160eda0affb0069c112c7474bc91559702d
Shawn Yan <shawn2016@aliyun.com>
2017-10-13 23:25:32 +0800

    session

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = { 
+    serverPort: '8765', 
+
+appId: 'wxeb5ae21053ad48e4', 
+    appSecret: '1f7023288c7b608c55fb7d648014a097', 
+
+// mongo
+mongoHost: '127.0.0.1', 
+    mongoPort: '27017', 
+    mongoUser: 'weapp', 
+    mongoPass: 'weapp-dev', 
+    mongoDb: 'weapp'
+};
